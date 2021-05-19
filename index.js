ee7d4724010cc170cc4d0b680ddf5672331f9829
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-19 16:06:51 +0600

    dhet

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-const { ObjectId } = require('bson');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const { ObjectId } = require("bson");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lkoox.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, It is working')
-})
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const serviceCollection = client.db("roxcePainting").collection("services");
-  
-  app.post('/addService', (req, res) => {
-      const data = req.body;
-      serviceCollection.insertOne(data)
-      .then(result=>{
-        console.log(result);
-        res.send(result.insertedCount > 0)
-        
-      })
-  })
-
-  app.get('/service', (req, res) => {
-    serviceCollection.find({})
-    .toArray((err, items) => {
-      console.log(err);
-      console.log(items);
-      res.send(items)
-    })
-  })
-
-  app.delete('/delete/:id', (req,res) =>{
-    const id = ObjectId(req.params.id);
-    serviceCollection.deleteOne({_id: id})
-    .then(document =>{
-      res.send(document)
-    })
-  })
-
-  app.get('/oneService/:id', (req, res) =>{
-    serviceCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, document)=>{
-      res.send(document)
-    })
-  })
-
+app.get("/", (req, res) => {
+  res.send("Hello, It is working");
 });
 
-app.listen(process.env.PORT || port)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const serviceCollection = client.db("roxcePainting").collection("services");
+  const orderList = client.db("roxcePainting").collection("orders");
+
+  app.post("/addService", (req, res) => {
+    const data = req.body;
+    serviceCollection.insertOne(data).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/service", (req, res) => {
+    serviceCollection.find({}).toArray((err, items) => {
+      console.log(err);
+      console.log(items);
+      res.send(items);
+    });
+  });
+
+  app.delete("/delete/:id", (req, res) => {
+    const id = ObjectId(req.params.id);
+    serviceCollection.deleteOne({ _id: id }).then((document) => {
+      res.send(document);
+    });
+  });
+
+  app.get("/oneService/:id", (req, res) => {
+    serviceCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, document) => {
+        res.send(document);
+      });
+  });
+
+  app.post("/placeOrder", (req, res) => {
+    const info = req.body;
+    orderList.insertOne(info).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.listen(process.env.PORT || port);
